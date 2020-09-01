4a00b0058dd319134f711f8153e785e94ed5d8f3
Justin Niessner <CrshOverride@gmail.com>
2020-09-01 15:37:20 -0400

    Strictly type the levels the logger will use

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export interface LoggerFunc {
-  (lvl: string, msg: string, meta?: object): void;
+  (lvl: 'debug' | 'info' | 'warn' | 'error', msg: string, meta?: object): void;
 }
 
 export { buildFullSpecificationList, buildPartialSpecificationList } from './specParser';
