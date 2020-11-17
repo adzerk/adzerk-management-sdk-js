d5d8ff04d7be6995167e2d0eb0c86ea0db9dc4f1
Justin Niessner <CrshOverride@gmail.com>
2020-11-16 21:12:30 -0500

    Add user management endpoints to spec parser

--- src/specParser.ts
+++ src/specParser.ts
@@ -69,6 +69,7 @@ export const buildFullSpecificationList = (
     `${basePath}/queued-report.yaml`,
     `${basePath}/scheduled-report.yaml`,
     `${basePath}/real-time-report.yaml`,
+    `${basePath}/user.yaml`,
   ];
 };
 
