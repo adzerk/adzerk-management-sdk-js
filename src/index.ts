3381e9d7fc8e61e2405501ed12b16b6dcedc81bd
Justin Niessner <CrshOverride@gmail.com>
2020-12-01 23:34:38 -0500

    Add empty enums to be filled later

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,34 @@ export interface LoggerFunc {
   (lvl: 'debug' | 'info' | 'warn' | 'error', msg: string, meta?: object): void;
 }
 
+export enum DistributionType {}
+
+export enum GoalType {}
+
+export enum RateType {}
+
+export enum CapType {}
+
+export enum FequencyCapType {}
+
+export enum CreativeTemplateFieldType {}
+
+export enum CreativeTemplateContentType {}
+
+export enum CreativeTemplateOperation {}
+
+export enum DayOfTheWeek {}
+
+export enum DupliateMode {}
+
+export enum DeliveryStatus {}
+
+export enum SelectionAlgorithm {}
+
+export enum ScchedulingWindow {}
+
+export enum RecurrenceType {}
+
 import {
   buildFullSpecificationList,
   buildPartialSpecificationList,
